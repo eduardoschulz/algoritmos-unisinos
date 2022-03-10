abf57c4df86c4e54bfbd19b0e73ee630f6f53df5
eduardoschulz <eduardo.schulz@protonmail.com>
2022-03-10 17:43:41 -0300

    corona diagnostico ...

--- aula4/corona.py
+++ aula4/corona.py
@@ -1,5 +1,55 @@
 #!/usr/bin/env python
-#Autor Eduardo Schu
-#Diagnosticando o Coronavírus
+# Calculadora
+# Eduardo Schulz
 
-print(8 * "=", " Diagnosticando o Coronavírus", "\n 1. Quais os Sintomas do Coronavírus  ")
+def soma():
+    num1 = int(input("Numero 1"))
+    num2 = int(input("Numero 1"))
+    num3 = (num1+num2)
+    print(num3)
+def subt():
+    num1 = int(input("Numero 1"))
+    num2 = int(input("Numero 1"))
+    num3 = (num1-num2)
+    print(num3)
+def mult():
+    num1 = int(input("Numero 1"))
+    num2 = int(input("Numero 1"))
+    num3 = (num1*num2)
+    print(num3)
+
+def div():
+    num1 = int(input("Numero 1"))
+    num2 = int(input("Numero 1"))
+    num3 = (num1/num2)
+    print(num3)
+def poten():
+    num1 = int(input("Numero 1"))
+    num2 = int(input("Numero 1"))
+    num3 = (num1**num2)
+    print(num3)
+
+def radia():
+    num1 = int(input("Numero 1"))
+    num2 = int(input("Numero 1"))
+    num3 = (num1**(1/num2))
+    print(num3)
+
+while(True):
+    print("======= Calculadora ======= \n 1 - Soma \n 2 - Subtração \n 3 - Multiplicação \n 4 - Divisão \n 5 - Potenciação \n 6 - Radiação \n 7 = sair")
+    num = int(input(""))
+    match num:
+        case 1:
+            soma()
+        case 2:
+            subt()
+        case 3:
+            mult()
+        case 4:
+            div()
+        case 5:
+            poten()
+        case 6:
+            radia()
+        case 7:
+            exit()
